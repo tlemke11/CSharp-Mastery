884b80de7273adf8ad3ccbc473613daf1bfc3f21
Tyler lemke <tylerlemke@Tylers-MacBook-Pro.local>
2018-02-15 20:12:03 -0600

    polymorphism example added

---
+++ Object-Oriented-Concepts/Polymorphism/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Vehicles;
+
+namespace PolyMorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+            Lambo lambo = new Lambo();
+            GeoMetro geometro = new GeoMetro();
+
+            List<Vehicle> vehicles = new List<Vehicle>();
+
+            vehicles.Add(lambo);
+            vehicles.Add(geometro);
+
+            foreach (Vehicle car in vehicles)
+            {
+                Console.WriteLine(car.honk());
+                Console.WriteLine(car.revEngine());
+            }
+        }
+    }
+}
---
+++ Object-Oriented-Concepts/Polymorphism/Vehicles.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Vehicles
+{
+    //inspired by https://videos.raywenderlich.com/courses/47-beginning-c/lessons/26
+
+    abstract class Vehicle
+    {
+        public abstract string honk();
+        public abstract string revEngine();
+    }
+
+    class Lambo : Vehicle
+    {
+        public override string honk()
+        {
+            return "TootToot!";
+        }
+
+        public override string revEngine()
+        {
+            return "VROOM!";
+        }
+    }
+
+    class GeoMetro : Vehicle
+    {
+        public override string honk()
+        {
+            return "tootle!";
+        }
+
+        public override string revEngine()
+        {
+            return "put put";
+        }
+    }
+}

2169eeed182331787c5f2be07f89905e2c853a29
Tyler Lemke <tyleris@gmail.com>
2018-03-02 23:54:46 -0600

    Starting Solid Principles

---
+++ Object-Oriented-Concepts/SOLID/SingleResponsibilityPrinciple/Example.cs
@@ -0,0 +1,24 @@
+﻿//A class should never have more than one reason to change - Uncle Bob
+
+
+//Don't Do THIS!
+using System;
+namespace Utils
+{
+    class Utils
+    {
+        private string hello = 'Hello Yall';
+
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public string sayHi()
+        {
+            return this.hello;
+        }
+    }
+}
+
+//Do this

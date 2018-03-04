cacee67a75ab986a95e03dbadc1d1a241a0587db
Tyler Lemke <tyleris@gmail.com>
2018-03-03 22:17:00 -0600

    code I forgot to push yesterday - and starting Design Patterns

--- Object-Oriented-Concepts/SOLID/SingleResponsibilityPrinciple/Example.cs
+++ Object-Oriented-Concepts/SOLID/SingleResponsibilityPrinciple/Example.cs
@@ -1,4 +1,13 @@
-﻿//A class should never have more than one reason to change - Uncle Bob
+﻿//THE GIST
+
+//A class should never have more than one reason to change - Uncle Bob
+
+
+//PROS of SRP
+//https://app.pluralsight.com/player?name=pood-single-responsibility-principle&clip=0&course=principles-oo-design&author=steve-smith
+//     -SRP enables higher cohesion and lower coupling.
+//     -SPR allows for more flexible design
+
 
 
 //Don't Do THIS!
